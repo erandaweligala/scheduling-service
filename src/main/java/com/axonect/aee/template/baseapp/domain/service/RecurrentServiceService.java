c65537281d0df1888697859dcd662206e8264904
Eranda_105689 <eranda.weligala@axiatadigitallabs.com>
2026-01-28 11:44:42 +0530

    first commit

--- src/main/java/com/axonect/aee/template/baseapp/domain/service/RecurrentServiceService.java
+++ src/main/java/com/axonect/aee/template/baseapp/domain/service/RecurrentServiceService.java
@@ -54,7 +54,10 @@ public class RecurrentServiceService {
     @Value("${recurrent-service.chunk-size}")
     private int chunkSize;
 
-    //todo need to implement first get services this filters apply ( NEXT_CYCLE_START_DATE = Tommorow and RECURRING_FLAG = 1 and not expired service EXPIRY_DATE   ) need get batch process
+    //todo need to implement first get services this filters apply
+    // ( NEXT_CYCLE_START_DATE = Tommorow and RECURRING_FLAG = 1 and not expired service EXPIRY_DATE   ) need get batch process
+    // need to ignore   userNamePage = userRepository.findUserNamesByStatus(UserStatus.ACTIVE, pageable); this part
+    // scenario is get servics and batch process related servis status
     public void reactivateExpiredRecurrentServices() {
         log.info("Reactivate expired recurrent services started..");
 
